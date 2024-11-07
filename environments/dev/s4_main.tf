bd02f018e0002853f5ebf6cc902e589f0b563379
SteveKimMHS <Steve.Kim@MHShealth.com>
2024-11-07 13:32:32 -0600

    add firewall rules

--- environments/dev/s4_main.tf
+++ environments/dev/s4_main.tf
@@ -1,3 +1,13 @@
+module "workloadidentity" {
+  source           = "../../modules/m4_workload_identity"
+  project_id = var.project_id
+  wi_pool_id = var.wi_pool_id
+  wi_pool_name = var.wi_pool_name
+  wi_pool_provider_id = var.wi_pool_provider_id
+  github_repository = var.github_repository
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.wi_sa_id}@${var.project_id}.iam.gserviceaccount.com"
+}
+
 module "vpc" {
   source                  = "../../modules/m1_vpc"
   network_name            = var.network_name
@@ -14,12 +24,31 @@ module "subnet" {
   project_id       = var.project_id
 }
 
-module "workloadidentity" {
-  source           = "../../modules/m4_workload_identity"
-  project_id = var.project_id
-  wi_pool_id = var.wi_pool_id
-  wi_pool_name = var.wi_pool_name
-  wi_pool_provider_id = var.wi_pool_provider_id
-  github_repository = var.github_repository
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.wi_sa_id}@${var.project_id}.iam.gserviceaccount.com"
+
+module "allow_http_https" {
+  source         = "../../modules/m3_firewall"
+  project_id     = var.project_id
+  rule_name      = "allow-http-https"
+  network        = module.vpc.network_self_link
+  direction      = "INGRESS"
+  priority       = 1000
+  allow_protocol = "tcp"
+  allow_ports    = ["80", "443"]
+  source_ranges  = ["0.0.0.0/0"]
+  target_tags    = ["web-server"]
 }
+
+module "allow_ssh" {
+  source         = "../../modules/m3_firewall"
+  project_id     = var.project_id
+  rule_name      = "allow-ssh"
+  network        = module.vpc.network_self_link
+  direction      = "INGRESS"
+  priority       = 1000
+  allow_protocol = "tcp"
+  allow_ports    = ["22"]
+  source_ranges  = ["0.0.0.0/0"]
+  target_tags    = ["ssh-access"]
+}
+
+
